22c57586df6ef5ca2f95966cd5dd8c25000dea06
Hariom Patidar <129758291+hariom24777@users.noreply.github.com>
2023-12-24 14:22:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+// DOM elements
+const body = document.querySelector("body"),
+    hourHand = document.querySelector(".hour"),
+    minuteHand = document.querySelector(".minute"),
+    secondHand = document.querySelector(".second"),
+    modeSwitch = document.querySelector(".mode-switch");
+
+// mode switch
+modeSwitch.addEventListener("click", () => {
+    body.classList.toggle("dark");
+
+    const isDarkMode = body.classList.contains("dark");
+    modeSwitch.textContent = isDarkMode ? "Light Mode" : "Dark Mode";
+
+    localStorage.setItem("mode", isDarkMode ? "Dark Mode" : "Light Mode")
+})
+
+if(localStorage.getItem("mode") === "Dark Mode"){
+    body.classList.add("dark");
+    modeSwitch.textContent = "Light Mode";
+}
+
+const updateTime = () =>{
+    // current time & calculate degree for clock hands
+    let date = new Date(),
+    secToDeg = (date.getSeconds() / 60) * 360,
+    minToDeg = (date.getMinutes() / 60) * 360,
+    hrToDeg = (date.getHours() / 12) * 360;
+
+    // rotate clock hands to the appropriate degree based on the current time
+    secondHand.style.transform = `rotate(${secToDeg}deg)`;
+    minuteHand.style.transform = `rotate(${minToDeg}deg)`;
+    hourHand.style.transform = `rotate(${hrToDeg}deg)`;
+}
+
+// call updateTime to set clock hands every second
+setInterval(updateTime, 1000);
+
+// call updateTime function on page load
+updateTime();
